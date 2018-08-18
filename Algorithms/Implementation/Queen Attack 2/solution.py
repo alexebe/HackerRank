e0bba4c251bd1bd051a2d24da4f61d15cf5f0509
alexebe <auguste.ebe@socitech.com>
2018-08-18 14:18:32 +0000

    Look through the obstacles and keep track of the closest ones. Passes all cases in O(k)

--- Algorithms/Implementation/Queen Attack 2/solution.py
+++ Algorithms/Implementation/Queen Attack 2/solution.py
@@ -8,27 +8,65 @@ import sys
 
 # Complete the queensAttack function below.
 def queensAttack(n, k, r_q, c_q, obstacles):
-    left = [(r_q,c) for c in range(c_q - 1, 0, -1)]
-    right = [(r_q,c) for c in range(c_q + 1, n + 1 , 1)]
-    bottom = [(r,c_q) for r in range(r_q - 1, 0, -1)]
-    top = [(r,c_q) for r in range(r_q + 1, n + 1, 1)]
-    diag_left_bottom = list(zip(range(r_q - 1, 0, -1), range(c_q - 1, 0, -1)))
-    diag_left_top = list(zip(range(r_q + 1, n + 1, 1), range(c_q - 1, 0, -1)))
-    diag_right_bottom = list(zip(range(r_q - 1, 0, -1), range(c_q + 1, n + 1, 1)))
-    diag_right_top = list(zip(range(r_q + 1, n + 1, 1), range(c_q + 1, n + 1, 1)))
+    #Closest obstacles on each direction
+    cObsLeft = [r_q, 0]
+    cObsRight = [r_q, n + 1]
+    cObsBottom = [0, c_q]
+    cObsTop = [n + 1, c_q]
     
-    listOfSquare = [left,right,bottom,top,diag_left_bottom,diag_left_top,diag_right_bottom,
-                   diag_right_top]
+    #Rows converge towards n + 1 while columns converge towards 0
+    #Do rows reach n + 1 before columns reach 0 or vice versa ?
+    cObsDiagLeftTop = [r_q + min(n + 1 - r_q, c_q),
+                      c_q - min(n + 1 - r_q, c_q)]
+    
+    #Both rows and columns converge towards 0
+    #Which of both dimensions reach 0 first ?
+    cObsDiagLeftBottom = [r_q - min(r_q, c_q), c_q - min(r_q, c_q)]
+    
+    #Rows converge towards 0 while columns converge towards n + 1
+    #Do rows reach 0 before columns reach n + 1 or vice versa ?
+    cObsDiagRightBottom = [r_q - min(r_q, n + 1 - c_q),
+                          c_q + min(r_q, n + 1 - c_q)]
+
+    #Both rows and columns converge towards n + 1
+    #Which of both dimensions reaches n + 1 first ?
+    cObsDiagRightTop = [r_q + min(n + 1 - r_q, n + 1 - c_q),
+                        c_q + min(n +1 - r_q, n +  1- c_q)]
+
+    for obs in obstacles:
+        if(obs[0] == r_q and obs[1] < c_q and obs[1] > cObsLeft[1]):
+            cObsLeft = obs
+        elif(obs[0] == r_q and obs[1] > c_q and obs[1] < cObsRight[1]):
+            cObsRight = obs
+        elif(obs[0] < r_q and obs[0] > cObsBottom[0] and obs[1] == c_q):
+            cObsBottom = obs
+        elif(obs[0] > r_q and obs[0] < cObsTop[0] and obs[1] == c_q):
+            cObsTop = obs
+		#Equation of NW diagonal is: Given a square situated a row r and column c,
+		#r - r_q = c_q - c and r - r_q > 0 and c_q - c > 0
+        elif(((obs[0] - r_q) == (c_q - obs[1])) and obs[0] - r_q > 0 
+             and c_q - obs[1] > 0 and cObsDiagLeftTop[0] > obs[0]):
+            cObsDiagLeftTop = obs
+		#Equation of SW diagonal is: Given a square situated a row r and column c,
+		#r_q - r = c_q - c and r_q - r > 0 and c_q - c > 0
+        elif(((r_q - obs[0]) == (c_q - obs[1])) and r_q - obs[0] > 0 
+             and c_q - obs[1] > 0 and cObsDiagLeftBottom[0] < obs[0]):  
+            cObsDiagLeftBottom = obs
+		#Equation of SE diagonal is: Given a square situated a row r and column c,
+		#r_q - r = c - c_q and r_q - r > 0 and c - c_q > 0
+        elif(((r_q - obs[0]) == (obs[1] - c_q)) and r_q - obs[0] > 0 
+             and obs[1] - c_q > 0 and cObsDiagRightBottom[0] < obs[0]):
+            cObsDiagRightBottom = obs
+		#Equation of NE diagonal is: Given a square situated a row r and column c,
+		#r - r_q = c - c_q and r - r_q > 0 and c - c_q > 0
+        elif(((obs[0] - r_q) == (obs[1] - c_q)) and obs[0] - r_q > 0 
+             and obs[1] - c_q > 0 and cObsDiagRightTop[0] > obs[0]):
+            cObsDiagRightTop = obs
+            
+    
     count = 0
-    for ls in listOfSquare:
-        count += attackableSquare(ls, obstacles)
-    return count    
-        
-def attackableSquare(listOfSquare, obstacles):
-    nb = 0
-    for s in listOfSquare:
-        if([s[0],s[1]] not in obstacles):
-            nb += 1
-        else:
-            break;
-    return nb        
+    count += cObsRight[1] - cObsLeft[1] - 2
+    count += cObsTop[0] - cObsBottom[0] - 2
+    count += cObsDiagLeftTop[0] - cObsDiagLeftBottom[0] - 2
+    count += cObsDiagRightTop[0] - cObsDiagRightBottom[0] - 2
+    return count
